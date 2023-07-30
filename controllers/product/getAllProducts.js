d4baf21cbe1b7a2d8cb68ddd7543f79d3cdcac5a
Rustem <dzhuraievrustem@gmail.com>
2023-07-30 23:54:16 +0200

    sorting

--- controllers/product/getAllProducts.js
+++ controllers/product/getAllProducts.js
@@ -3,6 +3,7 @@ const asyncHandler = require ('express-async-handler')
 
 const gelAllProducts = asyncHandler (async (req, res) => {
     try {
+        //Filtering
         const queryObj = {...req.query}
         const excludeFields = ["page", "sort", "limit", "fields"]
         excludeFields.forEach (el => delete queryObj[el])
@@ -12,6 +13,16 @@ const gelAllProducts = asyncHandler (async (req, res) => {
 
         const query = Product.find(JSON.parse(queryStr))
         const product = await query
+
+        //Sorting
+
+        if (req.query.sort) {
+            const sortBy = req.query.split(",").join(" ")
+            query = query.sort (sortBy)
+        } else {
+            query = query.sort("-createdAt")
+        }
+
         res.json(product)
     } catch (error) {
         throw new Error (error)
