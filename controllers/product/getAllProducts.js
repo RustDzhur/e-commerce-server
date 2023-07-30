b08286768ed28e3aa749e0936baf8d697bf25de1
Rustem <dzhuraievrustem@gmail.com>
2023-07-31 01:03:24 +0200

    limiting fields for products and pagination

--- controllers/product/getAllProducts.js
+++ controllers/product/getAllProducts.js
@@ -1,32 +1,50 @@
-const Product = require ('../../models/productModel')
-const asyncHandler = require ('express-async-handler')
+const Product = require("../../models/productModel");
+const asyncHandler = require("express-async-handler");
 
-const gelAllProducts = asyncHandler (async (req, res) => {
-    try {
-        //Filtering
-        const queryObj = {...req.query}
-        const excludeFields = ["page", "sort", "limit", "fields"]
-        excludeFields.forEach (el => delete queryObj[el])
+const gelAllProducts = asyncHandler(async (req, res) => {
+	try {
+		//Filtering
+		const queryObj = { ...req.query };
+		const excludeFields = ["page", "sort", "limit", "fields"];
+		excludeFields.forEach((el) => delete queryObj[el]);
 
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(
+			/\b(gte|gt|lte|lt)\b/g,
+			(match) => `$${match}`
+		);
 
-        const query = Product.find(JSON.parse(queryStr))
-        const product = await query
+		let query = Product.find(JSON.parse(queryStr));
 
-        //Sorting
+		//Sorting
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(",").join(" ");
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort("-createdAt");
+		}
 
-        if (req.query.sort) {
-            const sortBy = req.query.split(",").join(" ")
-            query = query.sort (sortBy)
-        } else {
-            query = query.sort("-createdAt")
-        }
+		//Limiting fields
+		if (req.query.fields) {
+			const fields = req.query.fields.split(",").join(" ");
+			query = query.select(fields);
+		}
 
-        res.json(product)
-    } catch (error) {
-        throw new Error (error)
-    }
-})
+		//Pagination
+		const page = req.query.page;
+		const limit = req.query.limit;
+		const skip = (page - 1) * limit;
+		query = query.skip(skip).limit(limit);
+		if (req.query.page) {
+			const productCount = await Product.countDocuments();
+			if (skip >= productCount) throw new Error("This page does not exists");
+		}
 
-module.exports = gelAllProducts
+		const product = await query;
+		res.json(product);
+	} catch (error) {
+		throw new Error(error);
+	}
+});
+
+module.exports = gelAllProducts;
