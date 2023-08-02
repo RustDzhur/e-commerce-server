827335a2210a23aee847e2aead6f0ee0217c06fe
Rustem <rustem@MacBook-Air-Rustem.local>
2023-08-02 05:22:40 +0200

    blog schema

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,27 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-});
+const blogSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		numViews: {
+			type: Number,
+			default: 0,
+		},
+		isLiked: {
+			type: Boolean,
+			default: false,
+		},
+		isDisliked: {
+			type: Boolean,
+			default: false,
+		},
+		likes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		dislikes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		image: {
+			type: String,
+			default:
+				"https://www.shutterstock.com/shutterstock/photos/381746308/display_1500/stock-photo-blog-blogging-homepage-social-media-network-concept-381746308.jpg",
+		},
+		author: {
+			type: String,
+			default: "Admin",
+		},
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
 
-
-module.exports = mongoose.model('Blog', blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
